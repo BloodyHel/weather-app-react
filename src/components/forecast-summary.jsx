df3ee16a9f17867cb5649dd02634a7285529fa4b
Helena Nicholson <nicholson.helena@gmail.com>
2018-05-13 15:47:00 +0100

    Formatting the icons and date

--- src/components/forecast-summary.jsx
+++ src/components/forecast-summary.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import WeatherIcon from 'react-icons-weather';
+import Moment from 'moment';
 
 const ForecastSummary = props => (
   <div className="forecast-summary">
-    <div className="forecast-summary__date"><span>{props.date}</span></div>
+    <div className="forecast-summary__date"><span>{moment(props.date).format('ddd Do MMM')}</span></div>
     <div className="forecast-summary__temperature"><span>{props.temperature}</span></div>
     <div className="forecast-summary__description"><span>{props.description}</span></div>
-    <div className="forecast-summary__icon"><span>{props.icon}</span></div>
+    <div className="forecast-summary__icon"><WeatherIcon name="owm" iconId={props.icon} /></div>
 
   </div>
 );
